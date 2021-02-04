906dcb05b0a48447a6ea9ce46086f8e4135d34b1
rezhaibrahim <rezhaibrahim96@gmail.com>
2021-02-04 16:51:50 +0700

    fix problem

--- src/redux/actions/auth.js
+++ src/redux/actions/auth.js
@@ -2,16 +2,16 @@ import http from '../../helpers/http'
 import qs from 'qs'
 import { default as axios } from 'axios'
 
-// const { REACT_APP_API_URL } = process.env
-// console.log("cek",REACT_APP_API_URL);
+const { REACT_APP_API_URL } = process.env
+console.log("cek",REACT_APP_API_URL);
 export default {
     login: (data) => ({
         type: 'LOGIN',
-        payload: axios.post(`http://18.139.50.74:8080/login`, qs.stringify(data)),
+        payload: http().post(`${REACT_APP_API_URL}login`, data),
     }),
     register: (data) => ({
         type: 'SIGNUP',
-        payload: axios.post(`/register`, qs.stringify(data)),
+        payload: http().post(`${REACT_APP_API_URL}register`,data),
     }),
     logout: () => ({
         type: 'LOGOUT',
